9393e9ddf3b56f35a9d1d17a129342d13fcb4d20
nagampere <yohei.naga@maas.co.jp>
2024-03-29 03:49:13 +0900

    intにintangible_assetsの追加

---
+++ projects/jip2023/dbt/models/intermediate/int_intangible_assets__nominal_original_entertainment_works.sql
@@ -0,0 +1,43 @@
+WITH unpivot_alias AS (
+    UNPIVOT {{ref('stg_intangible_assets__nominal_original_entertainment_works')}}
+    ON 
+        1994,
+        1995,
+        1996,
+        1997,
+        1998,
+        1999,
+        2000,
+        2001,
+        2002,
+        2003,
+        2004,
+        2005,
+        2006,
+        2007,
+        2008,
+        2009,
+        2010,
+        2011,
+        2012,
+        2013,
+        2014,
+        2015,
+        2016,
+        2017,
+        2018,
+        2019,
+        2020,
+        2021
+    INTO
+        NAME year
+        VALUE I_AO
+)
+
+SELECT 
+    ROW_NUMBER() OVER () as id,
+    section_id,
+    section_name,
+    year,
+    I_AO
+FROM unpivot_alias

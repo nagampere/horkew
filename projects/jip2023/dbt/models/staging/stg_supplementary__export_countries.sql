58aaf00c0568d84c4f00dd2e78d96a506852a0b6
nagampere <yohei.naga@maas.co.jp>
2024-03-31 22:18:48 +0900

    supplementary__exportの追加

---
+++ projects/jip2023/dbt/models/staging/stg_supplementary__export_countries.sql
@@ -0,0 +1,22 @@
+with source as (
+    SELECT 
+        ROW_NUMBER() OVER () as index,
+        * 
+    FROM st_read(
+        '../sources/supplementary_export.xlsx',
+        layer='Note',
+        open_options = ['HEADERS=DISABLE', 'FIELD_TYPES=STRING']
+    )
+)
+
+select
+    ROW_NUMBER() OVER () as id,
+    cast(Field1 as char) as area,
+    cast(Field2 as int) as country_id,
+    cast(Field3 as char) as country_name,
+    cast(Field8 as double) as country_name_ja,
+    cast(Field9 as double) as annotation
+from
+    source
+where
+    (index between 5 and 239) 
